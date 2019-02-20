51e7eb95337585e00fa8325acd1567916baa44d5
Ubuntu <ubuntu@ip-172-31-47-15.us-east-2.compute.internal>
2019-02-20 03:57:20 +0000

    auto deploy untuk autoscalling

---
+++ autodeploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+    echo "=============================>"
+    echo "Downloading Data"
+    echo "=============================>"
+    cd
+    rm -f master.zip
+    rm -R sosial-media-master
+    wget https://github.com/sdcilsy/sosial-media/archive/master.zip
+    echo "=============================>"
+    echo "Ekstrak File"
+    echo "=============================>"
+    unzip master.zip
+    echo "=============================>"
+    echo "Memindahkan data"
+    echo "=============================>"
+    sudo rm -R /var/www/html/*
+    sudo rm /var/www/html/*
+    sudo mv sosial-media-master/* /var/www/html/
+    echo "Setup selesai"
+    exit 0
